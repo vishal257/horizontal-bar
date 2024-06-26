4fbdee1a40fda0c811f297fe484ff07a10b847d2
Vishal <vishal2573003@gmail.com>
2024-06-26 19:49:46 +0530

    finished

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+import HorizontalScroll from './HorizontalScroll'
+
+function App() {
+  return(
+    <HorizontalScroll url={'https://dummyjson.com/products?limit=100'}/>
+  ) 
+}
+
+export default App
---
+++ src/HorizontalScroll.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+
+interface DataSet{
+  title: string
+}
+
+const HorizontalScroll = ({ url }: { url: string }) => {
+
+  const [data, setData] = useState<DataSet[]|null>(null);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [scrollPercentage, setScrollPercentage] = useState(0);
+
+  async function dummyContent() {
+    try {
+      setLoading(true);
+      setErrorMessage('');
+      const response = await fetch(url);
+      const result = await response.json();
+      if(result && result.products && result.products.length > 0){
+        setData(result.products);
+        setLoading(false);
+      }
+    } catch (error) {
+      setErrorMessage(String(error));
+    }
+  }
+
+  const handleScroller = () => {
+    const scrollTop = window.document.body.scrollTop || window.document.documentElement.scrollTop;
+    const scrollableHeight =  window.document.documentElement.scrollHeight - window.document.documentElement.clientHeight;
+    const percentage = (scrollTop / scrollableHeight) * 100; 
+    setScrollPercentage(percentage);
+  }
+
+  useEffect(() => {
+    dummyContent();
+  }, [url]);
+
+  useEffect(()=>{
+    window.addEventListener('scroll', handleScroller);
+    return () => window.removeEventListener('scroll', () => {})
+  },[])
+
+
+  if(errorMessage){
+    return(
+      <h1>An Error Occured</h1>
+    )
+  }
+
+  return (
+    <div className="container">
+    <div className="scroll-container">
+      <div><h1>Scroll Trigger</h1></div>
+      <div className="progress-gutter">
+        <div className="progress-bar" style={{width: `${scrollPercentage}%`}}></div>
+      </div>
+    </div>
+    <div className="content-container">
+     {data && data.length > 0 && !loading ? data.map((ele, index) => <h3 key={index}>{ele.title}</h3>): <h1>Loading content</h1>}
+    </div>
+    </div>
+  );
+};
+
+export default HorizontalScroll;
